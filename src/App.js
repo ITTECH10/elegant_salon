87c90e5e60003efdd82b950a33340ef31675f9a5
ITTECH10 <zivinice.connected@gmail.com>
2021-04-14 09:18:27 +0200

    fix counter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {Switch, Route} from 'react-router-dom'
 
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
+import Vehicles from './pages/Vehicles';
 
 function App() {
   return (
     <Layout>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/ponuda" component={Vehicles} />
       </Switch>
     </Layout>
   );
